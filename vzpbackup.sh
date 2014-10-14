32ca75e8e0d829ae766cb2f3c09ea7e1545c2850
Philipp Born <philipp@tamcore.eu>
2014-10-14 14:55:01 +0200

    remove hardcoded nice and ionice calls

--- vzpbackup.sh
+++ vzpbackup.sh
@@ -118,7 +118,7 @@ if [ "$FULL_BACKUP" = "yes" ]; then
   fi
 fi
 
-nice -n19 ionice -c3 rsync -avz -e "ssh -c arcfour" --{ignore-times,delete-before,inplace} $RSYNC_OPTS --exclude="/vz/*/*" /vz $DESTINATION
+rsync -avz -e "ssh -c arcfour" --{ignore-times,delete-before,inplace} $RSYNC_OPTS --exclude="/vz/*/*" /vz $DESTINATION
 
 if [ "$FULL_BACKUP" = "yes" ]; then
   ssh $( echo $DESTINATION | cut -d\: -f1 ) "find $( echo $DESTINATION | cut -d\: -f2 )/* -maxdepth 0 -iname '????.??.??' | head -n -$KEEP_COUNT | xargs rm -rf"
