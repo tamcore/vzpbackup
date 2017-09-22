104e0e27311d3727d0a1c703684c0ebf600ad913
TamCore <philipp@tamcore.eu>
2017-09-22 20:40:29 +0200

    vzpbackup: don't overwrite ${RSYNC_OPTS} if --keep-count is used

--- vzpbackup.sh
+++ vzpbackup.sh
@@ -132,7 +132,7 @@ if [ "${FULL_BACKUP}" = "yes" ]; then
   if (( ${KEEP_COUNT} > 0 )); then
     echo "KEEP_COUNT > 0; keeping backup.."
     REF_DATE=$( expr $( date --date="$( grep -m1 '<DateTime>' $(prlctl list -i ${VEID} | grep ^Home | cut -d' ' -f2)/Snapshots.xml | sed -rn 's/.*>(.*)<.*/\1/p' )" +%s ) - 86400 )
-    RSYNC_OPTS="--backup --backup-dir=$( date --date="@${REF_DATE}" +%Y.%m.%d )"
+    RSYNC_OPTS="${RSYNC_OPTS} --backup --backup-dir=$( date --date="@${REF_DATE}" +%Y.%m.%d )"
   fi
 fi
 
